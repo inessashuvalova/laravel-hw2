46ae44e1cc6841afb325e4a8bea14d4e5350cd4a
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2023-01-22 22:39:09 +0300

    fixed

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    use HasFactory;
+    protected  $table = 'todos';
+    protected $fillable = [
+        'title',
+        'description',
+    ];
 }
